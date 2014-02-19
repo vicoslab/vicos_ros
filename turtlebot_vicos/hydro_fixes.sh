0fd550234aa0daf8336554be20f44655b3cd4e73
Luka Čehovin <luka.cehovin@gmail.com>
2014-02-19 16:55:52 +0100

    Updating install scripts for hydro.

---
+++ turtlebot_vicos/hydro_fixes.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp /opt/ros/hydro/share/turtlebot_bringup/param/create/diagnostics.yaml /opt/ros/hydro/share/turtlebot_bringup/param/roomba
--- turtlebot_vicos/roshost.sh
+++ turtlebot_vicos/roshost.sh
@@ -1,6 +1,7 @@
-ROS_MASTER=/tmp/rosmaster.$USER
+#!/bin/bash
+ROS_MASTER=/tmp/roshost.$USER
 export ROS_IP=`ifconfig | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}' | head -1` 
-if [ -z "$1" ]; then 
+if [ -z "$1" ]; then
         if [ -f $ROS_MASTER ]; then 
         export ROS_MASTER_URI=`cat $ROS_MASTER` 
         else 
@@ -9,4 +10,8 @@ if [ -z "$1" ]; then
 else
 export ROS_MASTER_URI="http://${1}:11311"
 fi
-echo -n "$ROS_MASTER_URI" > $ROS_MASTER
+echo -n "$ROS_MASTER_URI" > "$ROS_MASTER"
+echo
+echo "ROS_IP is set to " $ROS_IP
+echo "ROS_MASTER_URI is set to" $ROS_MASTER_URI
+echo
